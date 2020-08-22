4488a63e950cee7a42ad30741b61048f3ec3d6b8
rishabh-kumar123 <69356371+rishabh-kumar123@users.noreply.github.com>
2020-08-22 19:19:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var car,wall;
+var speed,weight;
+
+
+
+function setup() {
+  createCanvas(1600,400);
+   car= createSprite(50, 200, 50, 50);
+   wall= createSprite(800, 200, 20, 400);
+   speed=random(55,90);
+   weight=random(400,1500);
+   car.velocityX=speed;
+}
+
+function draw() {
+  background("black"); 
+  
+if(wall.x-car.x<(car.width+wall.width)/2){
+
+car.velocityX=0;
+  deformation=0.5*weight*speed*speed/22500;
+
+   if(deformation<100){
+  car.shapeColor="green"
+} 
+ if(deformation>100 && deformation<180){
+  car.shapeColor="yellow"
+ } 
+if(deformation>180){
+car.shapeColor="red"
+}
+}
+  drawSprites();
+}
